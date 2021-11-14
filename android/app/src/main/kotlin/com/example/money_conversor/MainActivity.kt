73668113e561bfe17028b311583322181ef3937a
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-11-14 20:08:54 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/money_conversor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.money_conversor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/models/currency_model.dart
@@ -0,0 +1,9 @@
+class CurrencyModel {
+  final String name;
+  final double real;
+  final double dolar;
+  final double euro;
+  final double bitcoin;
+
+  CurrencyModel({this.name, this.real, this.dolar, this.euro, this.bitcoin});
+}
