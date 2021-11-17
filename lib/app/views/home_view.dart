887c5050931d896ba18621d14ec21d2885412af8
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-11-17 15:47:32 -0300

    align view items

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -16,28 +19,51 @@ class HomeView extends StatelessWidget {
               SizedBox(
                 width: MediaQuery.of(context).size.width,
                 child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
                       flex: 1,
-                      child: DropdownButton(
-                        items: const [],
+                      child: SizedBox(
+                        height: 57,
+                        child: DropdownButton(
+                          isExpanded: true,
+                          underline: Container(height: 1, color: Colors.amber),
+                          items: const [
+                            DropdownMenuItem(
+                              child: Text(
+                                'Real',
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    SizedBox(width: 10),
-                    Expanded(
+                    const SizedBox(width: 10),
+                    const Expanded(
                         flex: 2,
                         child: TextField(
-                          decoration: InputDecoration(),
+                          decoration: InputDecoration(
+                            enabledBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber),
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.amber),
+                            ),
+                          ),
                         )),
                   ],
                 ),
               ),
-              SizedBox(height: 50),
+              const SizedBox(height: 50),
               ElevatedButton(
                   onPressed: () {},
-                  child: Text(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.amber),
+                  ),
+                  child: const Text(
                     'CONVERTER',
-                  ))
+                  )),
             ],
           ),
         ),
